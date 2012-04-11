c6275a662c8ca67f25717050120cc61bd8ee1621
Dan Ivovich <dan@smartlogicsolutions.com>
2012-04-10 20:04:16 -0400

    RVM, xvfb init script, more packages, etc

--- chef-repo/cookbooks/packages/recipes/default.rb
+++ chef-repo/cookbooks/packages/recipes/default.rb
@@ -1,20 +1,33 @@
-package 'zsh' do
-  action :install
+
+# Shell stuff
+package 'zsh'
+package 'tmux'
+
+# Headless testing stuff
+package 'xvfb'
+package 'libqt4-dev'
+
+# Git
+package 'git-core'
+
+# Stuff to build stuff
+package 'curl'
+include_recipe 'build-essential'
+%w(libreadline-dev zlib1g-dev libssl-dev libxml2-dev libxslt1-dev libtool).each do |pkg|
+  package pkg
 end
 
-package 'tmux' do
-  action :install
+# typical project dependencies
+package 'pdftk'
+package 'xpdf'
+package 'libgif4'
+
+remote_file '/tmp/prince_8.0-1ubuntu10.04_amd64.deb' do
+  source 'http://www.princexml.com/download/prince_8.0-1ubuntu10.04_amd64.deb'
+  not_if 'test -f /tmp/prince_8.0-1ubuntu10.04_amd64.deb'
 end
 
-package 'xvfb' do
-  action :install
+package 'prince.deb' do
+  provider Chef::Provider::Package::Dpkg
+  source '/tmp/prince_8.0-1ubuntu10.04_amd64.deb'
 end
-
-package 'libqt4-dev' do
-  action :install
-end
-
-package 'git-core' do
-  action :install
-end
-
--- chef-repo/cookbooks/projects/recipes/default.rb
+++ chef-repo/cookbooks/projects/recipes/default.rb
@@ -1,7 +1,7 @@
 
 projects = data_bag('projects')
 
-projects.each do |project|
+projects.each_with_index do |project, index|
   attributes = data_bag_item('projects', project)
   home = "/home/#{project}"
   user(project) do
@@ -21,7 +21,6 @@ projects.each do |project|
       end
     end
   end
-  node['project_member_keys'] = keys
 
   directory "#{home}/.ssh" do
     action :create
@@ -35,6 +34,57 @@ projects.each do |project|
     owner project
     group project
     mode  '0600'
+    variables :keys => keys
+  end
+
+  execute "install rvm for #{project}" do
+    user project
+    group project
+    cwd home
+    environment ({'HOME' => home})
+    command "curl -L get.rvm.io | bash -s stable"
+    creates "#{home}/.rvm"
+    action :run
+  end
+
+  if attributes['ruby_version']
+    execute "install ruby #{attributes['ruby_version']} for #{project}" do
+      user project
+      group project
+      cwd home
+      environment ({'HOME' => home})
+      command "rvm install #{attributes['ruby_version']}"
+      not_if "rvm list | grep '#{attributes['ruby_version']}'"
+      action :run
+    end
+  end
+
+  (attributes['rubies'] || []).each do |ruby_version|
+    execute "install ruby #{ruby_version} for #{project}" do
+      user project
+      group project
+      cwd home
+      environment ({'HOME' => home})
+      command "rvm install #{ruby_version}"
+      not_if "rvm list | grep '#{ruby_version}'"
+      action :run
+    end
+  end
+
+  directory "/var/run/xvfb" do
+    action :create
+    mode '0755'
+  end
+
+  template "/etc/init.d/xvfb_#{project}" do
+    source 'xvfb_init.erb'
+    mode  '0755'
+    variables :display => "#{99 - index}", :project => project
+  end
+
+  execute "restart xvfb #{project}" do
+    command "sudo /etc/init.d/xvfb_#{project} restart"
+    action :run
   end
 end
 
@@ -42,3 +92,4 @@ group 'admin' do
   members projects
   append true
 end
+
