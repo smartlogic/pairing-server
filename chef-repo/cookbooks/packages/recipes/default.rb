b473f3ac4b0bf035f68b82e796c3f3a77f3d0657
Dan Ivovich <dan@smartlogicsolutions.com>
2012-07-25 10:18:51 -0400

    fix packaging changes for ubuntu 12.04

--- chef-repo/cookbooks/packages/recipes/default.rb
+++ chef-repo/cookbooks/packages/recipes/default.rb
@@ -12,13 +12,20 @@ package 'git-core'
 
 # Stuff to build stuff
 package 'curl'
+
 include_recipe 'build-essential'
 %w(libreadline-dev zlib1g-dev libssl-dev libxml2-dev libxslt1-dev libtool).each do |pkg|
   package pkg
 end
 
 package 'vim'
-package 'ctags'
+
+if node['platform_version'] == "12.04"
+  package 'exuberant-ctags'
+else
+  package 'ctags'
+end
+
 package 'ack-grep'
 
 # typical project dependencies
@@ -29,12 +36,23 @@ package 'redis-server'
 package 'memcached'
 
 package 'libssl0.9.8' # for prince to be happy
-remote_file '/tmp/prince_8.0-1ubuntu10.04_amd64.deb' do
-  source 'http://www.princexml.com/download/prince_8.0-1ubuntu10.04_amd64.deb'
-  not_if 'test -f /tmp/prince_8.0-1ubuntu10.04_amd64.deb'
-end
 
-package 'prince.deb' do
-  provider Chef::Provider::Package::Dpkg
-  source '/tmp/prince_8.0-1ubuntu10.04_amd64.deb'
+if node['platform_version'] == "12.04"
+  remote_file '/tmp/prince_8.1-1_ubuntu12.04_amd64.deb' do
+    source 'http://www.princexml.com/download/prince_8.1-1_ubuntu12.04_amd64.deb'
+    not_if 'test -f /tmp/prince_8.1-1_ubuntu12.04_amd64.deb'
+  end
+  package 'prince.deb' do
+    provider Chef::Provider::Package::Dpkg
+    source '/tmp/prince_8.1-1_ubuntu12.04_amd64.deb'
+  end
+else
+  remote_file '/tmp/prince_8.1-1_ubuntu10.04_amd64.deb' do
+    source 'http://www.princexml.com/download/prince_8.1-1_ubuntu10.04_amd64.deb'
+    not_if 'test -f /tmp/prince_8.1-1_ubuntu10.04_amd64.deb'
+  end
+  package 'prince.deb' do
+    provider Chef::Provider::Package::Dpkg
+    source '/tmp/prince_8.1-1_ubuntu10.04_amd64.deb'
+  end
 end
